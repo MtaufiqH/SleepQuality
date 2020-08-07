8c0b9aa9a51bb9f7ab78bec34243960a5b9561f2
M. Taufiq Hidayat <mtaufiqh62@gmail.com>
2020-08-07 15:03:37 +0800

    refactoring package

--- app/src/main/java/app/taufiq/trackmysleepquality/view/MainActivity.kt
+++ app/src/main/java/app/taufiq/trackmysleepquality/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package app.taufiq.trackmysleepquality
+package app.taufiq.trackmysleepquality.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import app.taufiq.trackmysleepquality.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
